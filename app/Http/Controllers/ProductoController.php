89c6f3156247755f1e08ccea4e0a0c63bae893b4
esteban1810 <estebansevillapg@gmail.com>
2021-04-15 19:57:16 -0500

    rutas

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -24,7 +24,7 @@ class ProductoController extends Controller
      */
     public function create()
     {
-        //
+        return view('producto/form');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/producto', function () {
+    return view('producto/index');
+});
+
+Route::get('/producto/create',[ProductoController::class,'create']);

7880465b27ec6ec4317a6b484e1f301e4554fa25
esteban1810 <estebansevillapg@gmail.com>
2021-04-15 20:02:29 -0500

    rutas (resource)

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,11 @@ use App\Http\Controllers\ProductoController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+/* 
 Route::get('/producto', function () {
     return view('producto/index');
 });
 
-Route::get('/producto/create',[ProductoController::class,'create']);
+Route::get('/producto/create',[ProductoController::class,'create']); */
+
+Route::resource('producto',ProductoController::class);
